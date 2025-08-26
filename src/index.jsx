745a0c6c8f2bd3d2b0edfcfffad219b0a0bb618c
Lucas Macaux <lucas0203@orange.fr>
2025-08-26 21:08:21 +0200

    ⚡️ Se débarasser de ReactDOM.render

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
 import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
