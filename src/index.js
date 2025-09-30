e3a0781732cc8b7940dfe17ddd0181c7a707c766
DonMako <80451849+DonMako@users.noreply.github.com>
2025-09-30 22:51:45 +0200

    Replace ReactDOM.render by createRoot.render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.scss';
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+createRoot(document.getElementById("root")).render(<App />);
